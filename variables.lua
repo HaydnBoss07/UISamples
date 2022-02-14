6712ddc8b165acb628e53917f0db29c5298e7026
HaydnBoss07 <99561098+HaydnBoss07@users.noreply.github.com>
2022-02-14 23:20:53 +0000

    Update variables.lua
    added a secondary list

--- variables.lua
+++ variables.lua
@@ -1,7 +1,8 @@
 client = game.Players.LocalPlayer
 nation = client.leaderstats.Country
 capital = workspace.CountryData[nation].Capital
-modlist = {286073467,165881970,178076512,36942410,731826417,582785312,889073499,199611834,42416882,33857533,197873406,63603700,40658655}
+modlist = {"M0tHeLL","ImNotACan","SmuthMudelar","halfmatthewhicat","Dadeylus","Bowniez","Green_Castaway","GrayshaValor","cadjosrez","Freasaloz","SykiRBLX","Robertmy6","Hyperant"}
+secondaryModlist = {
 whitelist = {} --scan through all games and get their jobIDs as long as a mod isnt there
 blacklist = {} --servers where mod is detected or someone threatens to report you
 --add value after these ^^^ whenever using them while programming
