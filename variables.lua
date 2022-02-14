4dfd30fa03a569af06f35879dea08f2d793ce7dd
HaydnBoss07 <99561098+HaydnBoss07@users.noreply.github.com>
2022-02-14 23:10:26 +0000

    Create variables.lua

---
+++ variables.lua
@@ -0,0 +1,6 @@
+local client = game.Players.LocalPlayer
+local nation = client.leaderstats.Country
+local capital = workspace.CountryData[nation].Capital
+local modlist = {286073467,165881970,178076512,36942410,731826417,582785312,889073499,199611834,42416882,33857533,197873406,63603700,40658655}
+--add value after these ^^^ whenever using them while programming
+--many scripts in this repository wont work without setVariables!!
